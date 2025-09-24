5b426eae5b666b91c93bea82cd851db0ea1a1fde
Ausaf-ul-Islam <ausafkhan7777@gmail.com>
2025-09-24 14:57:45 +0500

    second

--- config/inngest.js
+++ config/inngest.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import connectDB from "./db";
 import User from "@/models/User";
+import Order from "@/models/Order";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "quickcart-next" });
@@ -45,20 +46,48 @@ export const syncUserUpdate = inngest.createFunction(
       imageUrl: image_url,
     };
     await connectDB();
-    await User.findByIdAndUpdate(id, userData)
+    await User.findByIdAndUpdate(id, userData);
   }
 );
 
 // Inngest function to delete the user data from database
 export const syncUserDeletion = inngest.createFunction(
-    {
-        id: "delete-user-from-clerk"
-    },
-    {event:"clerk/user.deleted"},
-    async ({event})=>{
-        const {id} = event.data;
+  {
+    id: "delete-user-from-clerk",
+  },
+  { event: "clerk/user.deleted" },
+  async ({ event }) => {
+    const { id } = event.data;
 
-        await connectDB();
-        await User.findByIdAndDelete(id)
-    }
-)
+    await connectDB();
+    await User.findByIdAndDelete(id);
+  }
+);
+
+// Inngest function to create user's order in database
+export const createUserOrder = inngest.createFunction(
+  {
+    id: "create-user-order",
+    batchEvents: {
+      maxSize: 25,
+      timeout: "5s",
+    },
+  },
+  { event: "order/created" },
+  async ({ events }) => {
+    const orders = events.map((event) => {
+      return {
+        userId: event.data.userId,
+        items: event.data.items,
+        amount: event.data.amount,
+        address: event.data.address,
+        date: event.data.date,
+      };
+    });
+
+    await connectDB();
+    await Order.insertMany(orders);
+
+    return { success: true, processed: orders.length };
+  }
+);
